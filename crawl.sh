ffe22fc563e75230d60fbf7eb7a37ac70adce0f9
Jason Hall <jasonhall@redhat.com>
2022-03-01 09:03:38 -0500

    log less, save _version

--- crawl.sh
+++ crawl.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -euxo pipefail
+set -euo pipefail
 
 path=$(pwd)/gcloud/$(printf "%s/" "$@")
 mkdir -p $path
 
 if [[ $# -eq 0 ]] ; then
-  gcloud version > $(pwd)/gcloud/version
+  gcloud version > $(pwd)/gcloud/_version
   gcloud help > $(pwd)/gcloud/help
 fi
 
@@ -14,11 +14,13 @@ groups=$(gcloud help $@ | go run ./ -print=groups)
 commands=$(gcloud help $@ | go run ./ -print=commands)
 
 for grp in $groups; do
+	echo "$path$grp"
 	mkdir -p $path/$grp
 	gcloud help $@ $grp > $path/$grp/help
 	./crawl.sh $@ $grp
 done
 
 for cmd in $commands; do
+	echo "$path$cmd"
 	gcloud help $@ $cmd > $path/$cmd
 done
